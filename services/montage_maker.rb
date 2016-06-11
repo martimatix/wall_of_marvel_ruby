862f269944ddcce2a62a583d7789a8e351517a15
Mario Martinez <zenitram.oiram@gmail.com>
2016-06-11 19:09:44 +1000

    update magic numbers to constants

--- services/montage_maker.rb
+++ services/montage_maker.rb
@@ -3,12 +3,17 @@ require 'rmagick'
 class MontageMaker
   include Magick
 
+  COVERS_IN_MONTAGE = 12
+  JPG_COMPRESSION = 60
+  MONTAGE_BORDER_GEOMETRY = '+2+2'
+  MONTAGE_TILING = '6x2'
+
   def perform
-    image_list = Magick::ImageList.new(*Dir['covers/*.png'].sample(12))
+    image_list = Magick::ImageList.new(*Dir['covers/*.png'].sample(COVERS_IN_MONTAGE))
     collage = image_list.montage do |montage|
-      montage.geometry = '+2+2'
-      montage.tile = '6x2'
+      montage.geometry = MONTAGE_BORDER_GEOMETRY
+      montage.tile = MONTAGE_TILING
     end
-    collage.write('montage.jpg') { self.quality = 60 }
+    collage.write('montage.jpg') { self.quality = JPG_COMPRESSION }
   end
 end
